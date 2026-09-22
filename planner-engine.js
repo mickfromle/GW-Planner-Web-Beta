92727ac8192cd2e028ce722c47ececfcb564ba83
mickfromle <mnneustadt@gmail.com>
2026-09-22 22:55:51 +0200

    Deploy shared export time references

--- planner-engine.js
+++ planner-engine.js
@@ -1312,6 +1312,7 @@ window.GWPlannerEngine = (() => {
     hasPreferredSlot,
     timingPenalty,
     localTimeLabel,
+    viewerTimeLabel,
     utcTimeLabel,
     createAttackLoads,
     createTimeline,
